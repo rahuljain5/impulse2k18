5e7a8ebeed70a0aa6355e15a871794db93de6ece
rahuljain5 <rjain.rahul5@gmail.com>
2018-02-16 20:45:27 +0530

    doosra commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//app.js
+const models = require('../models');
+var express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+app.use(bodyParser.json());
+
+app.post("/register", function(req, res) {
+    register(req.body, (val) => {
+    res.send(val);
+  });
+})
+const register = (state, callback) => {
+	console.log(state);
+  models.User.create(state).then((val) => {
+    callback("REGISTRATION SUCCESSFUL)
+  }).catch((err) => {
+    callback("REGISTRATION FAILED")
+  })
+}
+
+models.sync();
+ app.listen(process.env.PORT, () => console.log("Server Started at " + process.env.PORT));
+
