14d83dac9492e591a91e62d0a1d6958cffcf2685
Shubham <shubham.399@gmail.com>
2018-02-18 21:04:50 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -3,23 +3,28 @@ module.exports = (sequelize, Sequelize) => {
   var User = sequelize.define("User", {
    name: {
       type: Sequelize.STRING,
-     defaultValue: null
+     defaultValue: null,
+      unique:"user"
     },
     eventName: {
       type: Sequelize.STRING,
-      defaultValue: null
+      defaultValue: null,
+      unique:"user"
     },
     email: {
       type: Sequelize.STRING,
-      defaultValue: null
+      defaultValue: null,
+      unique:"user"
     },
     PhoneNumber: {
       type: Sequelize.STRING,
-      defaultValue: null
+      defaultValue: null,
+      unique:"user"
     },
     collegeName:{
      type: Sequelize.STRING,
-      defaultValue: null
+      defaultValue: null,
+      unique:"user"
     }
   });
     return User;
