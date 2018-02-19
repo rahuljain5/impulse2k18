70cae1aa576df8f2f3ec669849d057f8c53c8f00
rahuljain5 <rjain.rahul5@gmail.com>
2018-02-19 10:22:52 +0530

    Front fixes, including event info

---
+++ js/event_data.js
@@ -0,0 +1,19 @@
+var eventname_desc = {
+    coding: {
+        name: "Coding",
+        desc: "<li>Two Members per team are allowed.</li><li>The event will be conducted over two rounds.</li><li>The first round will be a written debugging and coding round.</li><li>The final round will be a live coding round with 5 questions.</li><li> Solutions must be coded in C or C++ in GCC or G++ compiler.</li><li>Time limit:<br/>a. First Round: 40 minutes.<br/>b. Final Round: 90 minutes.</li><li>The team to finish first with the most efficient code will be declared winner.</li><li>Any attempt to cheat will lead to disqualification.</li><li>Judges decision will be final.</li>"
+    },
+    it_quiz:
+        { name: "IT Quiz" },
+    web_design:
+        { name: "Web Designing" },
+     debate: { name: "Debate" },
+     et_quiz: { name: "Entertainment Quiz" },
+     photography: { name: "Photography" },
+     treasure_hunt: { name: "Treasure Hunt" },
+     movie_making: { name: "Movie Making" },
+     namma_bhaashe_kannada: { name: "Namma Bhaashe Kannada" },
+     gaming: { name: "Gaming" },
+     footloose: { name: "FootLoose" },
+     buzzinga: { name: "Buzzinga" }
+};
