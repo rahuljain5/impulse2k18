d48f9bea83041063edaa754bcab116a5dc5aa288
rahuljain5 <rjain.rahul5@gmail.com>
2018-02-23 20:23:57 +0530

    Email message updated

--- utils/constant.js
+++ utils/constant.js
@@ -3,12 +3,13 @@ const mailcontent = `<!DOCTYPE html><html><head>
 </head>
 <body>
 <div class="container">
-<h2 style="font-size: 20px; font-weight: bold; margin: 0;">Reset Email Request</h2>
-<p>We received a request to reset the myDiary password for username <strong>@@@@</strong>.</p>
-<p>Please use <strong>####</strong> as a OTP to Reset your password.Your OTP will expire in <strong>%%%% </strong>mins.</p>
-<p>Try to Remember your password next time ;) well incase you forgot we got you covered</p>
-<p>If you didn't make this request, feel free to ignore this email.</p>
-</div>
+<h2 style="font-size: 20px; font-weight: bold; margin: 0;">Registration Successful</h2>
+<p>Dear <strong>@@@@</strong>,Thank you for registering for <strong>%%%% </strong>. Your registration has been received, thanks!.</p><br/>
+<p>If you have any questions leading up to the event, feel free to drop an email to <a href='mailto:toce.impulse@gmail.com'>toce.impulse@gmail.com</a> </p>
+<p>We look forward to seeing you on 22nd and 23rd of March!</p>
+<p>Venue: <br/><a href="https://www.google.co.in/maps/place/The+Oxford+College+of+Engineering/@12.9014963,77.63128,15z/data=!4m5!3m4!1s0x0:0xacb3790271972f97!8m2!3d12.9014963!4d77.63128?shorturl=1">The Oxford College of Engineering, Bommanahalli, Bangalore</a></p>
+</div><br/>
+<p>Thanks,<br/>Registration Team<br/>IMPULSE 2K18</p>
   </body>
 </html>`;
 
