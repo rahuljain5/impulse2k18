1238a60d9a85909cd4753a90d9b6572e61c97629
Shubham <shubham.399@gmail.com>
2018-03-17 15:19:32 +0530

    fix

--- app.js
+++ app.js
@@ -61,11 +61,21 @@ const cleanup = (s) =>{
 };
 const abbsearch = (s,query)=>{
 	let clgNamearr = s.split(",")[0].split(" ");
-	let filterarr = clgNamearr.filter(s => s.startsWith(query));
-	if(filterarr.length >0)
-	return true;
-	else
-		return false;
+	clgNamearr = clgNamearr.map(s=>s.charAt(0));
+	let apx = "";
+	for(i of clgNamearr)
+	{
+		if(i.match(/^[a-z]+$/i)
+		   apx += i
+	}
+	console.warn("Apx:"+apx);
+	return apx.startWith(query);
+// 	let filterarr = clgNamearr.filter(s => s.startsWith(query));
+// 	console.warn("Length:"+filterarr);
+// 	if(filterarr.length >0)
+// 	return true;
+// 	else
+// 		return false;
 
 }
 const getcollege = (state,callback)=>{
